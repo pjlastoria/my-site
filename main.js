e8f13b37a18e3ce03d0c64e40fe523c7f41a9117
pjlastoria <pjaylastoria@gmail.com>
2018-02-12 22:37:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,75 @@
-var body = document.querySelector("body");
-var navBtns = document.getElementsByClassName("nav-btns");
-var rightNavBtns = document.querySelector(".right");
-var aboutSection = document.getElementById("about");
-var menu = document.createElement("div");
-var navY = 50, projectSectH, projectSectY, projectSectScroll;
+var boxes = document.getElementsByClassName("box");
+var search = document.getElementById("search-box");
+var header = document.getElementById("header-text");
+search.addEventListener("click", handleSearchClick);
 
-window.addEventListener("scroll", updateProjectSectY);
+var sortTabs = document.getElementsByClassName("center-text");
+sortTabs[0].addEventListener("click", handleClick);
+sortTabs[1].addEventListener("click", handleClick);
+var currActive = 0;
 
-(function highlightNavBtns(i) {
-	if(i > 3) {return;}
-	
-	navBtns[i].addEventListener("click", scroll);
-	navBtns[i].addEventListener("mouseover", navLightToggle);
-	navBtns[i].addEventListener("mouseout", navLightToggle);
+var leftNavLinks = document.getElementsByClassName("left-nav-link");
+leftNavLinks = [].slice.call(leftNavLinks);
 
-	highlightNavBtns(++i);
-}(0))
+leftNavLinks.forEach(function(ele) {
+  ele.addEventListener("click", changeTitle)
+});
 
-function updateProjectSectY() {
-	projectSectH = window.getComputedStyle(aboutSection).height;
-	projectSectY = projectSectH.substring(0, projectSectH.length-2);
-	projectSectScroll = projectSectY-navY;
-}
-	
-function navLightToggle(ele) {
-	if(!ele.target.style.color) {
-		if(window.scrollY >= projectSectScroll) {
-			return ele.target.style.color = "#18CAE6";
-		}
-		return ele.target.style.color = "#173361";
-	}
-	ele.target.style.color = "";
+window.addEventListener('resize', updateGrid);
+
+function handleSearchClick() {
+  this.value = "";
 }
 
-function scroll(ele) {
-	if(ele.target.innerHTML === "Projects") {
-		return window.scrollTo(0, projectSectScroll);
-	}
-	
-	if(ele.target.innerHTML === "Contact") {
-		return window.scrollTo(0, document.body.scrollHeight); //bottom
-	}
-	
-	window.scrollTo(0, 0);
+/*function changeTitle(ele) {
+  var newTitle = ele.target.firstChild.innerHTML;
+  header.innerHTML = newTitle;
+}*/
+
+function handleClick(evt) {
+  if(evt.target.className === "top-nav-text center-text active") {
+    return;
+  }
+  
+  sortTabs[currActive].className = "top-nav-text center-text"
+  currActive = ( currActive === 0 ? 1 : 0 );
+  sortTabs[currActive].className = "top-nav-text center-text active";
+}
+
+function updateGrid() {
+  if(window.innerWidth < 1102 || window.innerWidth >= 1148 && window.innerWidth <= 1321) { // 4 based grid
+    return optimize4ColGrid();//928-1102 1148-1321 4grid, 1102-1147 5grid
+  }
+  
+  optimize5ColGrid(); // else, organize 5 based grid, no need to worry about 3 based or lower
+}
+
+function optimize5ColGrid() {
+  for(var i = 0; i < boxes.length; i++) {
+    if(i%5 === 0 || (i-1)%5 === 0) {
+      boxes[i].children[0].className = "movie-profile-right";
+      boxes[i].children[0].children[0].className = "left-arrow";
+    }
+    
+    if((i+3)%5 === 0 || (i+2)%5 === 0 || (i+1)%5 === 0) {
+      boxes[i].children[0].className = "movie-profile-left";
+      boxes[i].children[0].children[0].className = "right-arrow";
+    }
+  }
+}
+
+function optimize4ColGrid() {
+  
+  for(var i = 0; i < boxes.length; i++) {
+    if(i%4 === 0 || (i-1)%4 === 0) {
+      boxes[i].children[0].className = "movie-profile-right";
+      boxes[i].children[0].children[0].className = "left-arrow";
+    }
+    
+    if((i+2)%4 === 0 || (i+1)%4 === 0) {
+      boxes[i].children[0].className = "movie-profile-left";
+      boxes[i].children[0].children[0].className = "right-arrow";
+    }
+  }
+  
 }
